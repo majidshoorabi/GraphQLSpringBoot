52f56efffd3ff36259213e42067287ee4dc2126b
majijdshoorabi <majidshoorabi@gmail.com>
2022-07-02 08:42:33 +0430

    feat - add GraphQLSpringBootApplication.java

---
+++ src/main/java/com/github/majjidshoorabi/graphqlspringboot/GraphQLSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.majjidshoorabi.graphqlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQLSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQLSpringBootApplication.class, args);
+    }
+
+}
