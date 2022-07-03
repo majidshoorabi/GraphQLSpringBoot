feabcfefa40b1605d64fcbb2998c806e14490099
majijdshoorabi <majidshoorabi@gmail.com>
2022-07-03 08:50:41 +0430

    feat - Add ClientResolver

---
+++ src/main/java/com/github/majidshoorabi/graphqlspringboot/resolver/bank/ClientResolver.java
@@ -0,0 +1,30 @@
+package com.github.majidshoorabi.graphqlspringboot.resolver.bank;
+
+import com.github.majidshoorabi.graphqlspringboot.domain.banck.BankAccount;
+import com.github.majidshoorabi.graphqlspringboot.domain.banck.Client;
+import graphql.kickstart.tools.GraphQLResolver;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * @author majid.shoorabi
+ * @created 2022-03-July
+ * @project IntelliJ IDEA
+ */
+
+@Slf4j
+@Component
+public class ClientResolver implements GraphQLResolver<BankAccount> {
+
+    public Client client(BankAccount bankAccount) {
+        log.info("Requesting client data for bank account id {}", bankAccount.getId());
+
+        return Client.builder()
+                .id(UUID.randomUUID())
+                .firstname("Majid")
+                .lastname("Shoorabi")
+                .build();
+    }
+}
